88dec04fae48719fc4e67f2e027139765ea39be7
andhikareva <andhikaprtm4@gmail.com>
2020-04-15 14:29:22 +0700

    score challenge

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,24 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView winTeamText;
+    private TextView scorerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        winTeamText = findViewById(R.id.winText);
+        scorerName = findViewById(R.id.scorerNameInput);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            winTeamText.setText(extras.getString(MatchActivity.WIN_KEY));
+        }
     }
 }
